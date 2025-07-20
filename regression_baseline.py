dcac5edbf473fb30f8cef2d04a86b870a9f9b849
hd <hector@hds-MacBook-Pro.local>
2025-07-20 11:57:53 +0100

    model snapshot

--- regression_baseline.py
+++ regression_baseline.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d517802228d951fbbad01bce98b90618f67e6edcf91f01ee1d921f1b0162900
-size 23091
+oid sha256:81d72c38d7833baf1548db6d3ab69d1c768774a6b0eaece774d56637384c5649
+size 23537
